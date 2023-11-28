f74a9c28fcdb2f49a3278030c9c28a2540737294
Sergei Lavrentev <23312691+lavrd@users.noreply.github.com>
2023-11-28 12:44:44 +0400

    Update agreement contract naming

--- contracts/src/Agreement.sol
+++ contracts/src/Agreement.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+error ErrNotASigner();
+error ErrAlreadySigned();
+
 struct Agreement {
     address creator;
     address signer;
@@ -8,26 +11,23 @@ struct Agreement {
     bool isSigned;
 }
 
-error ErrNotASigner();
-error ErrAlreadySigned();
-
-contract AgreementContract {
+contract Contract {
     mapping(uint256 => Agreement) public agreements;
-    uint256 public nextAgreementId;
+    uint256 public nextId;
 
-    event AgreementCreated(uint256 agreementId, address signer);
-    event AgreementSigned(uint256 agreementId, address creator);
+    event Created(uint256 id, address signer);
+    event Signed(uint256 id, address creator);
 
-    function createAgreement(uint64 amount, address signer) public returns (uint256) {
-        uint256 agreementId = nextAgreementId;
-        agreements[agreementId] = Agreement(msg.sender, signer, amount, false);
-        emit AgreementCreated(agreementId, signer);
-        nextAgreementId++;
-        return agreementId;
+    function create(uint64 amount, address signer) public returns (uint256) {
+        uint256 id = nextId;
+        agreements[id] = Agreement(msg.sender, signer, amount, false);
+        emit Created(id, signer);
+        nextId++;
+        return id;
     }
 
-    function signAgreement(uint256 agreementId) public {
-        Agreement storage agreement = agreements[agreementId];
+    function sign(uint256 id) public {
+        Agreement storage agreement = agreements[id];
         if (agreement.isSigned) {
             revert ErrAlreadySigned();
         }
@@ -36,11 +36,11 @@ contract AgreementContract {
         }
         agreement.signer = msg.sender;
         agreement.isSigned = true;
-        emit AgreementSigned(agreementId, agreement.creator);
+        emit Signed(id, agreement.creator);
     }
 
-    function getAgreementById(uint256 agreementId) public view returns (Agreement memory) {
-        Agreement memory agreement = agreements[agreementId];
+    function getById(uint256 id) public view returns (Agreement memory) {
+        Agreement memory agreement = agreements[id];
         return agreement;
     }
 }
--- contracts/test/Agreement.t.sol
+++ contracts/test/Agreement.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import {AgreementContract, Agreement, ErrNotASigner, ErrAlreadySigned} from "../src/Agreement.sol";
+import {Contract as AgreementContract, Agreement, ErrNotASigner, ErrAlreadySigned} from "../src/Agreement.sol";
 import "forge-std/console.sol";
 import "forge-std/Vm.sol";
 
@@ -12,21 +12,25 @@ contract AgreementContractTest is Test {
     address constant SIGNER_ADDRESS = address(169);
     uint64 constant AMOUNT = 691;
 
+    // We need to copy that events from contract to avoid visibility errors.
+    event Created(uint256 id, address signer);
+    event Signed(uint256 id, address creator);
+
     function setUp() public {
         agreementContract = new AgreementContract();
     }
 
     function test_createAgreement() public {
         vm.expectEmit(true, true, false, true);
-        emit AgreementContract.AgreementCreated(0, SIGNER_ADDRESS);
-        uint256 agreementId = agreementContract.createAgreement(AMOUNT, SIGNER_ADDRESS);
+        emit Created(0, SIGNER_ADDRESS);
+        uint256 agreementId = agreementContract.create(AMOUNT, SIGNER_ADDRESS);
         assertEq(0, agreementId);
-        assertEq(1, agreementContract.nextAgreementId());
+        assertEq(1, agreementContract.nextId());
     }
 
     function test_getAgreement() public {
-        uint256 agreementId = agreementContract.createAgreement(AMOUNT, SIGNER_ADDRESS);
-        Agreement memory agreement = agreementContract.getAgreementById(agreementId);
+        uint256 agreementId = agreementContract.create(AMOUNT, SIGNER_ADDRESS);
+        Agreement memory agreement = agreementContract.getById(agreementId);
         console.log("Agreement creator:", agreement.creator);
         console.log("Agreement signer:", agreement.signer);
         console.log("Agreement amount:", agreement.amount);
@@ -38,28 +42,28 @@ contract AgreementContractTest is Test {
     }
 
     function test_signAgreement() public {
-        uint256 agreementId = agreementContract.createAgreement(AMOUNT, SIGNER_ADDRESS);
-        Agreement memory agreement_before = agreementContract.getAgreementById(agreementId);
+        uint256 agreementId = agreementContract.create(AMOUNT, SIGNER_ADDRESS);
+        Agreement memory agreement_before = agreementContract.getById(agreementId);
         assertEq(false, agreement_before.isSigned);
         vm.prank(SIGNER_ADDRESS);
         vm.expectEmit(true, true, false, true);
-        emit AgreementContract.AgreementSigned(0, address(this));
-        agreementContract.signAgreement(agreementId);
-        Agreement memory agreement_after = agreementContract.getAgreementById(agreementId);
+        emit Signed(0, address(this));
+        agreementContract.sign(agreementId);
+        Agreement memory agreement_after = agreementContract.getById(agreementId);
         assertEq(true, agreement_after.isSigned);
     }
 
     function test_RevertWhen_ContractAlreadySigned() public {
-        uint256 agreementId = agreementContract.createAgreement(AMOUNT, SIGNER_ADDRESS);
+        uint256 agreementId = agreementContract.create(AMOUNT, SIGNER_ADDRESS);
         vm.prank(SIGNER_ADDRESS);
-        agreementContract.signAgreement(agreementId);
+        agreementContract.sign(agreementId);
         vm.expectRevert(ErrAlreadySigned.selector);
-        agreementContract.signAgreement(agreementId);
+        agreementContract.sign(agreementId);
     }
 
     function test_RevertWhen_NotASigner() public {
-        uint256 agreementId = agreementContract.createAgreement(AMOUNT, SIGNER_ADDRESS);
+        uint256 agreementId = agreementContract.create(AMOUNT, SIGNER_ADDRESS);
         vm.expectRevert(ErrNotASigner.selector);
-        agreementContract.signAgreement(agreementId);
+        agreementContract.sign(agreementId);
     }
 }
