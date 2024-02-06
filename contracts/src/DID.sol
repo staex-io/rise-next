5ca26d52dcfb8a54c095d8fc2c9f817ea83a33fb
Sergei Lavrentev <23312691+lavrd@users.noreply.github.com>
2024-02-06 22:36:07 +0400

    Implement DID smart contract

---
+++ contracts/src/DID.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.22;
+
+struct DID {
+    string location;
+    uint256 price;
+}
+
+contract DIDContract {
+    mapping(address => DID) dids;
+
+    event Updated(address indexed, DID);
+    event Deleted(address indexed);
+
+    function update(string calldata location, uint256 price) external {
+        DID storage did = dids[msg.sender];
+        if (keccak256(abi.encodePacked(location)) != keccak256(abi.encodePacked(did.location))) {
+            did.location = location;
+        }
+        if (price != did.price) {
+            did.price = price;
+        }
+        dids[msg.sender] = did;
+        emit Updated(msg.sender, did);
+    }
+
+    // Delete did from smart contract storage and spawn a Deleted event.
+    function del() external {
+        delete dids[msg.sender];
+        emit Deleted(msg.sender);
+    }
+
+    function get(address entity) external view returns (DID memory) {
+        return dids[entity];
+    }
+}
